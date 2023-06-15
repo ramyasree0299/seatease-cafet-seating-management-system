d40bfb430562b8965f39fd15abb69addcf612535
D609409 <ramyasree.velchuri@jpmchase.com>
2023-06-15 13:48:11 +0530

    "Committing with instructions"

--- README.md
+++ README.md
@@ -22,32 +22,20 @@ cd Real-Time-Object-Detection-With-OpenCV
 ```
 **Step 3:** Install all the necessary libraries. I used MacOS for this project. These are some of the libraries I had to install:
 
+Run the following command for installing the requirements
 ```
-brew install opencv
-pip install opencv-python
-pip install opencv-contrib-python
-pip install opencv-python-headless
-pip install opencv-contrib-python-headless
-pip install matplotlib
-pip install imutils
+pip[pip3] install -r requirements.txt
 ```
 
 Make sure to download and install opencv and and opencv-contrib releases for OpenCV 3.3. This ensures that the deep neural network (dnn) module is installed. You must have OpenCV 3.3 (or newer) to run this code.
 
-**Step 4:** Make sure you have your video devices connected (e.g. Webcam, FaceTime HD Camera, etc.). You can list them by typing this in your terminal
-
-```
-system_profiler SPCameraDataType
-system_profiler SPCameraDataType | grep "^    [^ ]" | sed "s/    //" | sed "s/://"
-```
-
-**Step 5:** To start your video stream and real-time object detection, run the following command:
+**Step 4:** To start your video stream and real-time object detection, run the following command:
 
 ```
 python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
 ```
 
-**Step 6:** If you need any help regarding the arguments you pass, try:
+**Step 5:** If you need any help regarding the arguments you pass, try:
 
 ```
 python real_time_object_detection.py --help
--- real_time_object_detection.py
+++ real_time_object_detection.py
@@ -10,22 +10,25 @@ import argparse
 import imutils
 import time
 import cv2
-# from test_mysql_connection import mydb
 
+# Database code
+# ----------------------------
+# from test_mysql_connection import mydb
 # mycursor = mydb.cursor()
 # mycursor.execute("SELECT * FROM table_occupancy")
 # myresult = mycursor.fetchall()
 # for x in myresult:
 # 	print(x)
+# ----------------------------
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
+                help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
+                help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.2,
-	help="minimum probability to filter weak predictions")
+                help="minimum probability to filter weak predictions")
 args = vars(ap.parse_args())
 
 CLASSES = ["aeroplane", "background", "bicycle", "bird", "boat",
@@ -39,8 +42,12 @@ COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-#print("[INFO] starting video stream...")
-#vs = VideoStream(src=0).starts()
+# Live Stream code
+# ----------------------------
+# print("[INFO] starting video stream...")
+# vs = VideoStream(src=0).starts()
+# ----------------------------
+
 
 print("[INFO] Video Analysis Started...")
 vs = FileVideoStream(path="./media/cafet_6.MOV").start()
@@ -50,16 +57,15 @@ time.sleep(2.0)
 fps = FPS().start()
 
 objectCount = {
- "people":{
-	"currentCount": 0,
-	"prevCount": 0
-},
-	"chair":{
-		"currentCount": 0,
-		"prevCount": 0
-	}
+    "people": {
+        "currentCount": 0,
+        "prevCount": 0
+    },
+    "chair": {
+        "currentCount": 0,
+        "prevCount": 0
+    }
 }
-
 
 sleepInterval = 10
 
@@ -68,80 +74,76 @@ expectedChairs = 1
 chairsFoundCounterCheck = (60 / sleepInterval)
 # Todo: Send an event to mobile to start the 30 mins duration for TABLE XYZ here.
 while True:
-	objectCount["people"]["prevCount"] = objectCount["people"]["currentCount"]
-	objectCount["chair"]["prevCount"] = objectCount["chair"]["currentCount"]
-	objectCount["people"]["currentCount"] = 0
-	objectCount["chair"]["currentCount"] = 0
-	frame = vs.read()
-	print("The frame type: ",type(frame))
-	if frame is None:
-		break
-	frame = imutils.resize(frame, width=400)
-	(h, w) = frame.shape[:2]
-	resized_image = cv2.resize(frame, (300, 300))
-	blob = cv2.dnn.blobFromImage(resized_image, (1/127.5), (300, 300), 127.5, swapRB=True)
-	net.setInput(blob) # net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
-	predictions = net.forward()
+    objectCount["people"]["prevCount"] = objectCount["people"]["currentCount"]
+    objectCount["chair"]["prevCount"] = objectCount["chair"]["currentCount"]
+    objectCount["people"]["currentCount"] = 0
+    objectCount["chair"]["currentCount"] = 0
+    frame = vs.read()
+    print("The frame type: ", type(frame))
+    if frame is None:
+        break
+    frame = imutils.resize(frame, width=400)
+    (h, w) = frame.shape[:2]
+    resized_image = cv2.resize(frame, (300, 300))
+    blob = cv2.dnn.blobFromImage(resized_image, (1 / 127.5), (300, 300), 127.5, swapRB=True)
+    net.setInput(blob)  # net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+    predictions = net.forward()
 
-	# loop over the predictions
-	for i in np.arange(0, predictions.shape[2]):
-		# extract the confidence (i.e., probability) associated with the prediction
-		# predictions.shape[2] = 100 here
-		confidence = predictions[0, 0, i, 2]
-		# Filter out predictions lesser than the minimum confidence level
-		# Here, we set the default confidence as 0.2. Anything lesser than 0.2 will be filtered
-		if confidence > args["confidence"]:
-			# extract the index of the class label from the 'predictions'
-			# idx is the index of the class label
-			# E.g. for person, idx = 15, for chair, idx = 9, etc.
-			idx = int(predictions[0, 0, i, 1])
-			# then compute the (x, y)-coordinates of the bounding box for the object
-			box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
-			# Example, box = [130.9669733   76.75442174 393.03834438 224.03566539]
-			# Convert them to integers: 130 76 393 224
-			(startX, startY, endX, endY) = box.astype("int")
-			print("The co-ordinates ", (startX, startY, endX, endY) )
+    for i in np.arange(0, predictions.shape[2]):
+        confidence = predictions[0, 0, i, 2]
+        if confidence > args["confidence"]:
+            # extract the index of the class label from the 'predictions'
+            # idx is the index of the class label
+            # E.g. for person, idx = 15, for chair, idx = 9, etc.
+            idx = int(predictions[0, 0, i, 1])
+            # then compute the (x, y)-coordinates of the bounding box for the object
+            box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
+            # Example, box = [130.9669733   76.75442174 393.03834438 224.03566539]
+            # Convert them to integers: 130 76 393 224
+            (startX, startY, endX, endY) = box.astype("int")
+            print("The co-ordinates ", (startX, startY, endX, endY))
 
-			# Get the label with the confidence score
-			label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-			print("Object detected: ", label)
-			# Draw a rectangle across the boundary of the object
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				COLORS[idx], 2)
-			y = startY - 15 if startY - 15 > 15 else startY + 15
-			# Put a text outside the rectangular detection
-			# Choose the font of your choice: FONT_HERSHEY_SIMPLEX, FONT_HERSHEY_PLAIN, FONT_HERSHEY_DUPLEX, FONT_HERSHEY_COMPLEX, FONT_HERSHEY_SCRIPT_COMPLEX, FONT_ITALIC, etc.
-			cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-			if idx == 15:
-				objectCount["people"]["currentCount"] += 1
-			if idx == 9:
-				objectCount["chair"]["currentCount"] += 1
-	# show the output frame
-	cv2.imshow("Frame", frame)
+            # Get the label with the confidence score
+            label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+            print("Object detected: ", label)
+            # Draw a rectangle across the boundary of the object
+            cv2.rectangle(frame, (startX, startY), (endX, endY),
+                          COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            # Put a text outside the rectangular detection
+            # Choose the font of your choice: FONT_HERSHEY_SIMPLEX, FONT_HERSHEY_PLAIN, FONT_HERSHEY_DUPLEX, FONT_HERSHEY_COMPLEX, FONT_HERSHEY_SCRIPT_COMPLEX, FONT_ITALIC, etc.
+            cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+            if idx == 15:
+                objectCount["people"]["currentCount"] += 1
+            if idx == 9:
+                objectCount["chair"]["currentCount"] += 1
+    # show the output frame
+    cv2.imshow("Frame", frame)
 
-	key = cv2.waitKey(1) & 0xFF
-	# Todo: Sent event to the mobile application and refresh the table status at the application side.
-	print("The number of occupied seats:", objectCount["people"]["currentCount"])
-	print("The number of empty seats:", objectCount["chair"]["currentCount"])
+    key = cv2.waitKey(1) & 0xFF
+    # Todo: Sent event to the mobile application and refresh the table status at the application side.
+    print("The number of occupied seats:", objectCount["people"]["currentCount"])
+    print("The number of empty seats:", objectCount["chair"]["currentCount"])
 
-	# Check every 5 seconds
-#	time.sleep(sleepInterval)
-	
-	if (expectedChairs == objectCount["chair"]["currentCount"]) and (expectedChairs == objectCount["chair"]["prevCount"]):
-		chairsFoundCounterCheck -= 1
-		# Todo: Send an event that the last 1 minute the expected Chairs are available to be occupied.
-	else: 
-		chairsFoundCounterCheck =  (60 / sleepInterval)
-	if chairsFoundCounterCheck == 0:
-		print("Empty table ready to be occupied")
-		chairsFoundCounterCheck =  (60 / sleepInterval)
-	print("The chairs found counter check: ", chairsFoundCounterCheck)
-	# Press 'q' key to break the loop
-	if key == ord("q"):
-		break
+    # Check every 5 seconds
+    #	time.sleep(sleepInterval)
 
-	# update the FPS counter
-	fps.update()
+    if (expectedChairs == objectCount["chair"]["currentCount"]) and (
+            expectedChairs == objectCount["chair"]["prevCount"]):
+        chairsFoundCounterCheck -= 1
+    # Todo: Send an event that the last 1 minute the expected Chairs are available to be occupied.
+    else:
+        chairsFoundCounterCheck = (60 / sleepInterval)
+    if chairsFoundCounterCheck == 0:
+        print("Empty table ready to be occupied")
+        chairsFoundCounterCheck = (60 / sleepInterval)
+    print("The chairs found counter check: ", chairsFoundCounterCheck)
+    # Press 'q' key to break the loop
+    if key == ord("q"):
+        break
+
+    # update the FPS counter
+    fps.update()
 
 # stop the timer
 fps.stop()
@@ -155,9 +157,7 @@ cv2.destroyAllWindows()
 # Stop the video stream
 vs.stop()
 
-
-
-
 # cd downloads/real-time-object-detection/cafeteria-management-system
 # python3 real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
 # /usr/local/mysql/bin/mysql -uroot -p
+# python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
